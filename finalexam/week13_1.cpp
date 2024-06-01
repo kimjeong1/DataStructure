735b4a69639d0cec730abb7df59ae110d1db4f3d
kimjeong_1 <59051200+kimjeong1@users.noreply.github.com>
2024-06-01 17:34:25 +0900

    Create week13_1.cpp

---
+++ finalexam/week13_1.cpp
@@ -0,0 +1,137 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+pair<bool, int> vertices[10002];
+bool graph[10002][10002];
+
+void insertVertex(int s) {
+    if (vertices[s].first) {
+        cout << "Exist\n";
+        return;
+    }
+    vertices[s].first = true;
+}
+
+void insertEdge(int s, int d) {
+    if (graph[s][d]) {
+        cout << "Exist\n";
+        return;
+    }
+    graph[s][d] = true;
+    graph[d][s] = true;
+    vertices[s].second++;
+    vertices[d].second++;
+}
+
+void eraseVertex(int s) {
+    vertices[s].first = false;
+    for (int i = 1; i <= 10000; i++) {
+        if (graph[s][i]) {
+            graph[s][i] = false;
+            vertices[s].second--;
+        }
+        if (graph[i][s]) {
+            graph[i][s] = false;
+            vertices[i].second--;
+        }
+
+    }
+}
+
+void eraseEdge(int s, int d) {
+    if (!graph[s][d]) {
+        cout << "None\n";
+        return;
+    }
+    graph[s][d] = false;
+    graph[d][s] = false;
+    vertices[s].second--;
+    vertices[d].second--;
+}
+
+void isAdjacent(int s, int d) {
+    if (graph[s][d] == true) {
+        cout << "True ";
+    }
+    else {
+        cout << "False ";
+    }
+    if (vertices[s].second == vertices[d].second) cout << min(s, d) << '\n';
+    else if (vertices[s].second < vertices[d].second) cout << s << '\n';
+    else cout << d << '\n';
+}
+
+void minVertexInfo2() {
+    pair<int, int> ans = make_pair(-1, 10001);
+    for (int i = 0; i <= 10000; i++) {
+        if (!vertices[i].first) continue;
+        if (vertices[i].first > ans.first) {
+            ans.first = i;
+            ans.second = vertices[i].second;
+        }
+    }
+
+
+    if (ans.first == -1) cout << "-1\n";
+    else cout << ans.first << ' ' << ans.second << '\n';
+}
+
+void minVertexInfo() {
+    pair<int, int> ans = make_pair(-1, 10001);
+    for (int i = 0; i <= 10000; i++) {
+        if (!vertices[i].first) continue;
+        if (vertices[i].second < ans.second) {
+            ans.first = i;
+            ans.second = vertices[i].second;
+        }
+    }
+    if (ans.second == 0)
+        ans.first = -1;
+
+
+    if (ans.first == -1) cout << "-1\n";
+    else cout << ans.first << ' ' << ans.second << '\n';
+}
+
+int main() {
+    int t;
+
+    cin >> t;
+
+    for (int i = 0; i < t; i++) {
+        string cmd;
+        int s, d;
+        cin >> cmd;
+        if (cmd == "insertVertex") {
+            cin >> s;
+            insertVertex(s);
+
+        }
+        else if (cmd == "insertEdge") {
+            cin >> s >> d;
+            insertEdge(s, d);
+
+        }
+        else if (cmd == "eraseVertex") {
+            cin >> s;
+            eraseVertex(s);
+        }
+        else if (cmd == "eraseEdge") {
+            cin >> s >> d;
+            eraseEdge(s, d);
+
+        }
+        else if (cmd == "isAdjacent") {
+            cin >> s >> d;
+            isAdjacent(s, d);
+
+        }
+        else if (cmd == "minVertexInfo") {
+            minVertexInfo2();
+        }
+    }
+
+    return 0;
+}
