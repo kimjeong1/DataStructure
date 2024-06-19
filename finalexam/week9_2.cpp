e8f4816fb252b7899e28dfe823f7db4ea94d249f
kimjeong_1 <59051200+kimjeong1@users.noreply.github.com>
2024-06-19 22:12:45 +0900

    Update week9_2.cpp

--- finalexam/week9_2.cpp
+++ finalexam/week9_2.cpp
@@ -4,92 +4,56 @@
 using namespace std;
 
 struct compare {
-	bool operator()(const int& lhs, const int& rhs) {
-
-		int count = 0;
-		int l = lhs;
-		int r = rhs;
-		while (1) {
-			l /= 10;
-			r /= 10;
-			if (l == 0 || r == 0) break;
-
-		}
-		if (l == 0 && r == 0) return lhs > rhs;
-
-		else return lhs < rhs;
-	}
+    bool operator()(const int& lhs, const int& rhs) {
+        int l = lhs;
+        int r = rhs;
+        while (1) {
+            l /= 10;
+            r /= 10;
+            if (l == 0 || r == 0) {
+                break;
+            }
+        }
+        if (l == 0 && r == 0) return lhs > rhs;
+        else return lhs < rhs;
+    }
 };
 
-class sortedSeqPQ {
-private:
-	compare C;
-	vector<int> arr;
-public:
-	int size() {
-		return arr.size();
-	}
-	bool empty() {
-		return size() == 0;
-	}
+void insertion(vector<int>& arr) {
+    compare c;
+    for (int i = 1; i < arr.size(); i++) {
+        int key = arr[i];
+        int j = i - 1;
 
-	void push(int e) {
-		int idx;
-		for (idx = 0; idx < arr.size(); idx++) {
-			if (C(arr[idx], e)) break;
-		}
-		arr.insert(arr.begin() + idx, e);
-	}
-	int top() {
-		if (empty()) return -1;
-
-		return arr.back();
-	}
-	void pop() {
-		if (empty()) return;
-
-		arr.pop_back();
-	}
-	vector<int> insertionSort() {
-		vector<int> sort;
-		while (!empty()) {
-			sort.push_back(top());
-			pop();
-		}
-		return sort;
-	}
-};
+        while (j >= 0 && c(key, arr[j])) {
+            arr[j + 1] = arr[j];
+            j -= 1;
+        }
+        arr[j + 1] = key;
+    }
+}
 
 int main() {
-	int t;
-	cin >> t;
+    int t;
+    cin >> t;
 
-	for (int i = 0; i < t; i++) {
-		int n;
-		cin >> n;
+    for (int i = 0; i < t; i++) {
+        int n;
+        cin >> n;
 
-		sortedSeqPQ pq;
+        vector<int> s(n);
 
+        for (int i = 0; i < n; i++) {
+            cin >> s[i];
+        }
 
+        insertion(s);
 
-		for (int j = 0; j < n; j++) {
-			int x;
-			cin >> x;
-			pq.push(x);
-		}
-
-
-		cout << "Case #" << i + 1 << ".\n";
-
-
-		vector<int> s = pq.insertionSort();
-		for (int num : s) {
-			cout << num << ' ';
-		}
-
-		cout << "\n";
-
-	}
-
-	return 0;
+        cout << "Case #" << i + 1 << ".\n";
+        for (int j = 0; j < n; j++) {
+            cout << s[j] << ' ';
+        }
+        cout << '\n';
+    }
+    return 0;
 }
